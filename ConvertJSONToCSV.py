bb31b10c40a694c9580ea1576bc41228f5d55e95
RickvdHeijden <76222429+RickvdHeijden@users.noreply.github.com>
2024-05-29 21:40:16 +0200

    feature: Improved json_to_csv code

--- ConvertJSONToCSV.py
+++ ConvertJSONToCSV.py
@@ -1,32 +1,9 @@
-import json
-import csv
-from pathlib import Path
+import pandas as pd
 
 
 def json_to_csv(json_file, csv_file):
-    # If csv file already exists, return
-    csv_file = Path(csv_file)
-    if csv_file.exists():
-        print(f'CSV file {csv_file} already exists!')
-        return
-    # Define the CSV file headers
-    headers = ['title', 'author', 'rank', 'amazon_product_url']
-
-    # Open CSV file for writing
-    with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        # Write the headers
-        csvwriter.writerow(headers)
-
-        # Read JSON data line by line
-        with open(json_file, 'r', encoding='utf-8') as f:
-            for line in f:
-                entry = json.loads(line)
-                row = [
-                    entry.get('title', ''),
-                    entry.get('author', ''),
-                    entry['rank'].get('$numberInt', '') if 'rank' in entry else '',
-                    entry.get('amazon_product_url', '')
-                ]
-                csvwriter.writerow(row)
+    df = pd.read_json(json_file, lines=True, dtype_backend='pyarrow', engine='pyarrow')
+    df = df[['title', 'author', 'rank', 'amazon_product_url']]
+    df['rank'] = df['rank'].apply(lambda x: x.get('$numberInt', None) if x else None)
+    df.to_csv(csv_file, index=False)
     print(f'CSV file {csv_file} created successfully!')
