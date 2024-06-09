7f613dcf471c53d0167b28010a4d262bf296fb7d
Andreas Efstathiou <95447662+Skydrite@users.noreply.github.com>
2024-06-09 12:52:50 +0200

    Changes to enricher.py to (1) log some statistics to see how far we are in the enriching process and (2) save and load intermediate results, as this is gonna take a while

--- non_best_selling_books/enricher.py
+++ non_best_selling_books/enricher.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests
+import os
+import time
 
 """
 Goal: Enrich the non-best selling books dataset by fetching more info on them.
@@ -9,9 +11,8 @@ Its usage comes after the dataset is built -> retriever.py
 
 
 def extract_isbn(book):
-    isbns = book.get('isbn', [])
-    isbn13 = next((isbn for isbn in isbns if len(isbn) == 13), None)
-    isbn10 = next((isbn for isbn in isbns if len(isbn) == 10), None)
+    isbn13 = book.get('isbn13')
+    isbn10 = book.get('isbn10')
     return isbn13, isbn10
 
 
@@ -32,14 +33,40 @@ def fetch_additional_details(isbn):
     return {}
 
 
+def load_intermediate_results(file_path):
+    if os.path.exists(file_path):
+        return pd.read_csv(file_path)
+    print("Creating a new dataframe")
+    return pd.DataFrame(columns=['title', 'author', 'isbn13', 'isbn10', 'rating', 'description',
+                                 'publisher', 'categories', 'country_of_publication'])
+
+
+def save_intermediate_results(data, save_path):
+    data.to_csv(save_path, index=False)
+
+
 if __name__ == '__main__':
-    # Read the non_best_selling_books.csv file
-    non_best_selling_books_df = pd.read_csv('../datasets/non_best_selling_books.csv')
+    non_best_selling_books_path = '../datasets/non_best_selling_books_filtered.csv'
+    intermediate_results_path = '../datasets/intermediate_enriched_non_best_selling_books.csv'
+    final_results_path = '../datasets/enriched_non_best_selling_books.csv'
+
+    # Read the filtered non_best_selling_books.csv file
+    non_best_selling_books_df = pd.read_csv(non_best_selling_books_path)
+
+    # Load previously saved intermediate results
+    enriched_books_df = load_intermediate_results(intermediate_results_path)
+    enriched_books_titles = set(enriched_books_df['title'].unique())
 
     # List to hold updated book information
-    enriched_books = []
+    enriched_books = enriched_books_df.to_dict('records')
+
+    api_calls_count = 0
+    start_time = time.time()
 
     for _, book in non_best_selling_books_df.iterrows():
+        if book['title'] in enriched_books_titles:
+            continue
+
         isbn13, isbn10 = extract_isbn(book)
 
         # If any field is missing, fetch additional details
@@ -59,8 +86,28 @@ if __name__ == '__main__':
                 book['country_of_publication'] = additional_details.get('country_of_publication',
                                                                         book['country_of_publication'])
 
-        enriched_books.append(book)
+                # Track the number of API calls
+                api_calls_count += 1
+
+                # Print metrics every 50 API calls
+                if api_calls_count % 50 == 0:
+                    end_time = time.time()
+                    duration_minutes = (end_time - start_time) / 60.0
+                    api_calls_per_minute = api_calls_count / duration_minutes
+                    print(f"{api_calls_count} API calls so far")
+                    print(f"API calls per minute: {api_calls_per_minute:.2f}")
+
+                # Save intermediate results every 100 API calls
+                if api_calls_count % 100 == 0:
+                    enriched_books_df = pd.DataFrame(enriched_books)
+                    save_intermediate_results(enriched_books_df, intermediate_results_path)
+
+                # Adding a delay to avoid hitting the API rate limit
+                time.sleep(1)
+
+        enriched_books.append(book.to_dict())
 
     # Convert list to DataFrame and write to CSV
     enriched_books_df = pd.DataFrame(enriched_books)
-    enriched_books_df.to_csv('../datasets/enriched_non_best_selling_books.csv', index=False)
+    save_intermediate_results(enriched_books_df, final_results_path)
+    print(f"Enriched non-best-selling books saved to '{final_results_path}'")
