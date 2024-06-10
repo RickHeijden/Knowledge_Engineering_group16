ee884d7d7fcfc049a73bd188a55f5b36247538de
Andreas Efstathiou <95447662+Skydrite@users.noreply.github.com>
2024-06-10 20:43:49 +0200

    Added year to our latest dataset

---
+++ combiner/year_adder.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def add_year_from_processed():
+    # Step 1: Read the CSV files
+    processed_df = pd.read_csv('../datasets/processed.csv')
+    combined_filtered_enriched_df = pd.read_csv('../datasets/combined_filtered_enriched.csv')
+
+    # Step 2: Merge the DataFrames on the 'title' column
+    result_df = pd.merge(combined_filtered_enriched_df, processed_df[['title', 'year']], on='title', how='left')
+
+    # Step 3: Save the resulting DataFrame to a new CSV file
+    result_df.to_csv('processed_combined_filtered_enriched.csv', index=False)
+
+
+if __name__ == "__main__":
+    add_year_from_processed()
