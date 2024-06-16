933aa42279bba92c8bf779093f38da27b544a1c5
RickvdHeijden <76222429+RickvdHeijden@users.noreply.github.com>
2024-06-16 16:50:23 +0200

    docs: Documented data_retriever

--- data_retriever.py
+++ data_retriever.py
@@ -5,13 +5,26 @@ from requests import Response
 
 
 class DataRetriever:
+    """
+    Class that retrieves data from the Google Books API and the DBpedia SPARQL endpoint.
+    """
+
+    # Number of requests made to the API
     __requests: int
 
     def __init__(self):
         self.__requests = 0
 
     def get_json_from_isbn(self, isbn: str) -> dict | bool:
+        """
+        Get the JSON response from the Google Books API using the ISBN.
+
+        @param isbn: The ISBN of the book.
+
+        @return: The JSON response from the Google Books API.
+        """
         self.__requests += 1
+        # Rate limit the requests
         if self.__requests % 10 == 0:
             time.sleep(1)
 
@@ -24,6 +37,7 @@ class DataRetriever:
             }
 
             response: Response = requests.get(base_api_link, params=params)
+
             if response.status_code != 200:
                 return False
 
@@ -34,7 +48,16 @@ class DataRetriever:
             return False
 
     def get_json_from_title_and_author(self, title: str, author: str | None) -> dict | bool:
+        """
+        Get the JSON response from the Google Books API using the title and author.
+
+        @param title: The title of the book.
+        @param author: The author of the book.
+
+        @return: The JSON response from the Google Books API.
+        """
         self.__requests += 1
+        # Rate limit the requests
         if self.__requests % 10 == 0:
             time.sleep(1)
 
@@ -54,6 +77,7 @@ class DataRetriever:
                 }
 
             response: Response = requests.get(base_api_link, params=params)
+
             if response.status_code != 200:
                 return False
 
@@ -64,7 +88,15 @@ class DataRetriever:
             return False
 
     def get_books_from_author(self, author_name: str) -> list:
+        """
+        Get the books written by the author from the Google Books API.
+
+        @param author_name: The name of the author to retrieve the books for.
+
+        @return: The list of books written by the author.
+        """
         self.__requests += 1
+        # Rate limit the requests
         if self.__requests % 10 == 0:
             time.sleep(1)
 
@@ -73,6 +105,7 @@ class DataRetriever:
         books = []
 
         try:
+            # Looping over the pagination
             while start_index == 0 or start_index < total_items:
                 base_api_link = "https://www.googleapis.com/books/v1/volumes"
                 params = {
@@ -81,17 +114,23 @@ class DataRetriever:
                     'startIndex': start_index,
                     'projection': 'lite'
                 }
+
                 response = requests.get(base_api_link, params=params)
+
                 if response.status_code != 200:
                     return []
+
                 data = response.json()
 
                 if 'totalItems' not in data or 'items' not in data:
                     return books
+
                 if not total_items:
                     total_items = data['totalItems']
+
                 books.extend(data['items'])
                 start_index += 40
+
             return books
         except RequestException as e:
             print(f"An error occurred: {e}")
@@ -99,7 +138,21 @@ class DataRetriever:
             return []
 
     def get_author_info_from_dbpedia(self, author_name: str) -> dict | bool:
+        """
+        Get the author information from DBpedia using the author name.
+
+        We retrieve the following information about the author if available:
+        - Abstract
+        - Birth date
+        - Country name
+        - Death date
+        - Genres the author writes in
+
+        @param author_name: The name of the author to retrieve the information for.
+        @return: The JSON response from the DBpedia SPARQL endpoint.
+        """
         self.__requests += 1
+        # Rate limit the requests
         if self.__requests % 100 == 0:
             time.sleep(1)
 
@@ -173,8 +226,3 @@ class DataRetriever:
             print(f"An error occurred: {e}")
 
             return False
-
-
-if __name__ == '__main__':
-    data_retriever = DataRetriever()
-    print(data_retriever.get_books_from_author('JJ Smith'))
