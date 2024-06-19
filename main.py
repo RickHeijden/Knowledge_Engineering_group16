88f507288518d9c2793775327d4046ff9736e94a
RickvdHeijden <76222429+RickvdHeijden@users.noreply.github.com>
2024-06-19 22:27:27 +0200

    refactor: Integration to function

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ import os
 from cleaner import clean_authors
 from combiner import *
 
+
+def integrate_dataframes():
+    """
+    Integrate the dataframes into a single dataframe.
+
+    @return: The integrated dataframe.
+    """
+    generation = GenerateDataframes(directory)
+    df_file1, df_file2, df_file3, df_file4 = generation.generate_dataframes()
+
+    dataframe = combine_dataframes(df_file1, df_file2, df_file3, df_file4)
+
+    print(len(dataframe))
+    # Join on isbn10
+    dataframe = combine_on_isbn10(dataframe)
+    print(len(dataframe))
+
+    # Join on isbn13
+    print(len(dataframe))
+    dataframe = combine_on_isbn13(dataframe)
+
+    # Lowercase the title
+    dataframe['title'] = dataframe['title'].str.lower()
+
+    # Join on title and author
+    dataframe = combine_on_title_author(dataframe)
+    print(len(dataframe))
+
+    dataframe.to_csv(directory + 'combined.csv', index=False)
+
+    return dataframe
+
+
 # 1) Convert the fourth dataset from JSON file to CSV
 # 2) Explore the csv headers
 # 3) Combine the 4 CSV files into a single CSV file
@@ -13,28 +46,7 @@ if __name__ == '__main__':
 
     # Check if combined.csv exists using os
     if not os.path.exists(directory + 'combined.csv'):
-        generateDataframes = GenerateDataframes(directory)
-        df_file1, df_file2, df_file3, df_file4 = generateDataframes.generate_dataframes()
-
-        combined_dataframe = combine_dataframes(df_file1, df_file2, df_file3, df_file4)
-
-        print(len(combined_dataframe))
-        # Join on isbn10
-        combined_dataframe = combine_on_isbn10(combined_dataframe)
-        print(len(combined_dataframe))
-
-        # Join on isbn13
-        print(len(combined_dataframe))
-        combined_dataframe = combine_on_isbn13(combined_dataframe)
-
-        # Lowercase the title
-        combined_dataframe['title'] = combined_dataframe['title'].str.lower()
-
-        # Join on title and author
-        combined_dataframe = combine_on_title_author(combined_dataframe)
-        print(len(combined_dataframe))
-
-        combined_dataframe.to_csv(directory + 'combined.csv', index=False)
+        combined_dataframe = integrate_dataframes()
     else:
         combined_dataframe = pd.read_csv('datasets/combined.csv')
 
